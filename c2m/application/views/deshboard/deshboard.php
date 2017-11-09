ec28987a3dc49d45d3796e5fc5fe4b7e1bb93036
Yogolas <yogolaspas@gmail.com>
2017-11-09 13:03:05 +0700

    Update deshboard.php

--- c2m/application/views/deshboard/deshboard.php
+++ c2m/application/views/deshboard/deshboard.php
@@ -35,16 +35,6 @@
 
 
 
-<a href="<?php echo $base_url;?>/mycustomer" class="btn btn-primary" style="font-size: 20px;font-weight: bold; width: 200px;">
-<span class="glyphicon glyphicon-user" aria-hidden="true" style="font-size: 90px;"></span><br /> <?=$lang_cusandcontact?>	
-</a>
-
-
-
-<a href="<?php echo $base_url;?>/sale/salelist" class="btn btn-danger disabled"  style="font-size: 20px;font-weight: bold; width: 200px;">
-<span class="glyphicon glyphicon-list-alt" aria-hidden="true" style="font-size: 90px;"></span><br /> <?=$lang_salereport?>	
-</a>
-
 
 <hr />
 
